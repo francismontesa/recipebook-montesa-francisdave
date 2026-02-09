7ebb9158fe117da8b026176bd3deb337fdc856cc
francismontesa <francis.montesa@student.ateneo.edu>
2026-02-09 20:09:39 +0800

    Added ingredient list to recipe list

--- ledger/views.py
+++ ledger/views.py
@@ -4,21 +4,21 @@ import ast
 
 
 def recipe_list(request):
-    ctx = get_context('ledger/contexts/Recipe List Context.txt')
+    ctx = get_context_from_file('ledger/contexts/Recipe List Context.txt')
     return render(request, 'ledger/recipe_list.html', ctx)
 
 
 def recipe1(request):
-    ctx = get_context('ledger/contexts/Recipe 1.txt')
+    ctx = get_context_from_file('ledger/contexts/Recipe 1.txt')
     return render(request, 'ledger/recipe.html', ctx)
 
 
 def recipe2(request):
-    ctx = get_context('ledger/contexts/Recipe 2.txt')
+    ctx = get_context_from_file('ledger/contexts/Recipe 2.txt')
     return render(request, 'ledger/recipe.html', ctx)
 
 
-def get_context(file_dir):
+def get_context_from_file(file_dir):
     context_file = open(file_dir, 'r')
     context_content = context_file.read()
     context_file.close()
