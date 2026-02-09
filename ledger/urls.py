e4d97bfaa6cfb8fa4153300c3bcbbbdb4bd9822a
francismontesa <francis.montesa@student.ateneo.edu>
2026-02-09 13:20:59 +0800

    Renamed imports

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import recipeList, recipe1, recipe2
+from .views import recipe_list, recipe1, recipe2
 
 urlpatterns = [
-    path('recipes/list', recipeList, name='list'),
+    path('recipes/list', recipe_list, name='list'),
     path('recipe/1', recipe1, name='recipe1'),
     path('recipe/2', recipe2, name='recipe2'),
 ]
