bb297be72674f715732e2f0e3f9c4e946a2e1a4c
francismontesa <francis.montesa@student.ateneo.edu>
2026-02-09 09:33:58 +0800

    configured urls

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from .views import recipeList, recipe1, recipe2
 
 urlpatterns = [
-    path('/recipes/list', recipeList, name='list'),
-    path('/recipe/1', recipe1, name='recipe1'),
-    path('/recipe/2', recipe2, name='recipe2'),
+    path('recipes/list', recipeList, name='list'),
+    path('recipe/1', recipe1, name='recipe1'),
+    path('recipe/2', recipe2, name='recipe2'),
 ]
+
+app_name = 'ledger'
--- ledger/views.py
+++ ledger/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def recipeList():
+def recipeList(request):
     return HttpResponse("recipe list")
 
-def recipe1():
+def recipe1(request):
     return HttpResponse("recipe 1")
 
-def recipe2():
+def recipe2(request):
     return HttpResponse("recipe 2") 
