c8de0c336ce8ca61975c76d626ee3dec67c786aa
francismontesa <francis.montesa@student.ateneo.edu>
2026-02-09 13:18:14 +0800

    Cleaned views.py

--- ledger/views.py
+++ ledger/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 import ast
 
-def readContextFile(fileDir):
-    contextFile = open(fileDir, 'r')
-    contextContent = contextFile.read()
-    contextFile.close()
-    return ast.literal_eval(contextContent)
 
-def recipeList(request):
-    ctx = readContextFile('ledger/contexts/Recipe List Context.txt')
+def recipe_list(request):
+    ctx = get_context('ledger/contexts/Recipe List Context.txt')
     return render(request, 'ledger/recipe_list.html', ctx)
 
+
 def recipe1(request):
-    ctx = readContextFile('ledger/contexts/Recipe 1.txt')
+    ctx = get_context('ledger/contexts/Recipe 1.txt')
     return render(request, 'ledger/recipe.html', ctx)
 
+
 def recipe2(request):
-    ctx = readContextFile('ledger/contexts/Recipe 2.txt')
+    ctx = get_context('ledger/contexts/Recipe 2.txt')
     return render(request, 'ledger/recipe.html', ctx)
+
+
+def get_context(file_dir):
+    context_file = open(file_dir, 'r')
+    context_content = context_file.read()
+    context_file.close()
+    return ast.literal_eval(context_content)
