81e6d8df4d34dd15f176626e833477ae6e1c7ec1
francismontesa <francis.montesa@student.ateneo.edu>
2026-02-19 21:04:05 +0800

    Added string representation and absolute urls

--- ledger/models.py
+++ ledger/models.py
@@ -1,13 +1,23 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return '{}'.format(self.name)
+
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+    def get_absolute_url(self):
+        return reverse('recipe', args=[str(self.name)])
 
 
 class RecipeIngredient(models.Model):
