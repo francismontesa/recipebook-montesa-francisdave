b525766f36ace5e3f48b169b0738016690f74206
francismontesa <francis.montesa@student.ateneo.edu>
2026-03-02 15:31:43 +0800

    Added Profile model

--- ledger/models.py
+++ ledger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -35,3 +36,9 @@ class RecipeIngredient(models.Model):
 
     def __str__(self):
         return '{} {} {}'.format(self.quantity, self.ingredient, self.recipe)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(blank=True)
