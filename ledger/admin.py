8d0b93123a78ea1b6f72f35b94761813de0f2198
francismontesa <francis.montesa@student.ateneo.edu>
2026-03-12 12:02:34 +0800

    Modified RecipeAdmin to include RecipeImageInline

--- ledger/admin.py
+++ ledger/admin.py
@@ -2,17 +2,21 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import Recipe, RecipeIngredient, Profile
+from .models import Recipe, RecipeIngredient, RecipeImage, Profile
 
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
 
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
+
+
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
     readonly_fields = ('created_on', 'update_on',)
-    inlines = [RecipeIngredientInline, ]
+    inlines = [RecipeIngredientInline, RecipeImageInline,]
 
 
 class ProfileInline(admin.StackedInline):
