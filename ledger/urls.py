d03387a476af6d9fa5d163f3741571042a102875
francismontesa <francis.montesa@student.ateneo.edu>
2026-03-12 18:36:03 +0800

    Added forms for creating new recipes and adding images

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import recipe_list, recipe
+from .views import recipe_list, recipe, recipe_form, image_form
 
 urlpatterns = [
     path('recipes/list', recipe_list, name='list'),
     path('recipe/<int:id>', recipe, name='recipe'),
+    path('recipe/add', recipe_form, name='add'),
+    path('recipe/<int:id>/add_image', image_form, name='add-image')
 ]
 
 app_name = 'ledger'
--- ledger/views.py
+++ ledger/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Recipe
+from .forms import RecipeForm, RecipeImageForm
 
 
 def recipe_list(request):
@@ -14,3 +15,28 @@ def recipe_list(request):
 def recipe(request, id):
     ctx = {'recipe': Recipe.objects.get(id=id)}
     return render(request, 'ledger/recipe.html', ctx)
+
+
+def recipe_form(request):
+    form = RecipeForm()
+    if request.method == 'POST':
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            new_recipe = form.save()
+            return redirect('ledger:recipe', id=new_recipe.id)
+    ctx = {"create_form": form}
+    return render(request, 'ledger/recipe_form.html', ctx)
+
+
+@login_required(redirect_field_name=None)
+def image_form(request, id):
+    form = RecipeImageForm()
+    if request.method == 'POST':
+        form = RecipeImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.recipe = Recipe.objects.get(id=id)
+            image.save()
+            return redirect('ledger:recipe', id=id)
+    ctx = {'create_form': form, 'id': id}
+    return render(request, 'ledger/image_form.html', ctx)
